f432f027b9b4fae65088dd86dccb960246e7f8eb
zhfg <angell.zhfg@gmail.com>
2020-03-11 00:03:52 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+func getKubernetesClient() (kubernetes.Interface, myresourceclientset.Interface) {
+	// construct the path to resolve to `~/.kube/config`
+	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
+
+	// create the config from the path
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
+
+	// generate the client based off of the config
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
+
+	myresourceClient, err := myresourceclientset.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
+
+	log.Info("Successfully constructed k8s client")
+	return client, myresourceClient
+}
