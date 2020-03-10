938297a14e958038759b24909a728e88046cf911
zhfg <angell.zhfg@gmail.com>
2020-03-11 00:04:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -22,3 +22,20 @@ func getKubernetesClient() (kubernetes.Interface, myresourceclientset.Interface)
 	log.Info("Successfully constructed k8s client")
 	return client, myresourceClient
 }
+
+func main() {
+	// get the Kubernetes client for connectivity
+	client, myresourceClient := getKubernetesClient()
+
+	// retrieve our custom resource informer which was generated from
+	// the code generator and pass it the custom resource client, specifying
+	// we should be looking through all namespaces for listing and watching
+	informer := myresourceinformer_v1.NewMyResourceInformer(
+		myresourceClient,
+		meta_v1.NamespaceAll,
+		0,
+		cache.Indexers{},
+	)
+
+	// ... remainder of main.main unchanged and omitted for brevity
+}
