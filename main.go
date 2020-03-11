6c5f46dd308c1b8fa974c22991b5936f0bc0361b
jacob <jacob.zhang@connext.com.cn>
2020-03-11 10:45:50 +0800

    add controller

--- main.go
+++ main.go
@@ -24,12 +24,13 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog"
+	// "k8s.io/kubernetes/staging/src/k8s.io/metrics/pkg/client/clientset_generated/clientset"
 	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	//_ "github.com/zhfg/k8s-crd-debug/pkg/signals"
 
 	clientset "github.com/zhfg/k8s-crd-debug/pkg/client/clientset/versioned"
 	informers "github.com/zhfg/k8s-crd-debug/pkg/client/informers/externalversions"
-	"github.com/zhfg/k8s-crd-debug/pkg/signals"
 )
 
 var (
@@ -42,38 +43,44 @@ func main() {
 	flag.Parse()
 
 	// set up signals so we handle the first shutdown signal gracefully
-	stopCh := signals.SetupSignalHandler()
+	//stopCh := signals.SetupSignalHandler()
 
 	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
 	if err != nil {
 		klog.Fatalf("Error building kubeconfig: %s", err.Error())
 	}
 
+	klog.Info(cfg)
+
 	kubeClient, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
 		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
 	}
 
+	klog.Info(kubeClient)
+
 	exampleClient, err := clientset.NewForConfig(cfg)
 	if err != nil {
 		klog.Fatalf("Error building example clientset: %s", err.Error())
 	}
-
+	klog.Info(exampleClient)
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
 	exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+	klog.Info(kubeInformerFactory)
+	klog.Info(exampleInformerFactory)
 
-	controller := NewController(kubeClient, exampleClient,
-		kubeInformerFactory.Apps().V1().Deployments(),
-		exampleInformerFactory.Samplecontroller().V1alpha1().Foos())
+	//controller := NewController(kubeClient, exampleClient,
+	//	kubeInformerFactory.Apps().V1().Deployments(),
+	//	exampleInformerFactory.Samplecontroller().V1alpha1().Foos())
 
 	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
 	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
-	kubeInformerFactory.Start(stopCh)
-	exampleInformerFactory.Start(stopCh)
+	//kubeInformerFactory.Start(stopCh)
+	//exampleInformerFactory.Start(stopCh)
 
-	if err = controller.Run(2, stopCh); err != nil {
-		klog.Fatalf("Error running controller: %s", err.Error())
-	}
+	//if err = controller.Run(2, stopCh); err != nil {
+	//	klog.Fatalf("Error running controller: %s", err.Error())
+	//}
 }
 
 func init() {
